0761e8c2f393f151fd4d9d737e25f3b161e1c563
antepavlinusic <ante.pavlinusic@gmail.com>
2024-05-27 23:41:17 +0200

    Vjezbanje taksisluzba
    Vjezbanje kucanje koda

---
+++ taksisluzba.sql
@@ -0,0 +1,29 @@
+--create database taksisluzba;
+
+
+
+--create table vozaci(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--oib varchar(11),
+--kaznjavan bit
+--);
+
+--create table putnik(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--brojtelefona varchar(20),
+--);
+
+--create table voznja(
+--sifra int,
+--cijena decimal(18,2),
+--pocetna_adresa varchar(50),
+--zavrsna_adresa varchar(50),
+--kilometri decimal(10,1)
+--);
+
+
+
