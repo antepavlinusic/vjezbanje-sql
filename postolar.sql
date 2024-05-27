24a2521f6b97fbe8cb853aebba4ab9c9da4e96c6
antepavlinusic <ante.pavlinusic@gmail.com>
2024-05-27 23:40:08 +0200

    Vjezbanje postolar

---
+++ postolar.sql
@@ -0,0 +1,30 @@
+--create database postolar;
+
+
+--create table obuca(
+--sifra int, 
+--model varchar(40),
+--boja varchar(40),
+--materijal varchar(40)
+--);
+
+--create table segrt(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--oib varchar(11)
+--);
+
+--create table popravak(
+--sifra int,
+--cijena decimal(18,2),
+--mjesto_ostecenja varchar(20),
+--segrt int
+--);
+
+
+
+--create table korisnik_usluge(
+--obuca int,
+--popravak int
+--);
