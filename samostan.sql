deb26f23387f288ca99a3cc451d0c34c7209ca9a
antepavlinusic <ante.pavlinusic@gmail.com>
2024-05-27 23:42:14 +0200

    Vjezbanje samostan
    Vjezbanje kucanje koda

---
+++ samostan.sql
@@ -0,0 +1,16 @@
+--create database samostan;
+
+--create table svecenici(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--oib varchar(11)
+--);
+
+--create table posao(
+--sifra int,
+--naziv varchar(50),
+--lokacija varchar(50),
+--datum_i_vrijeme datetime
+--);
+
