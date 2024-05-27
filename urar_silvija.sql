f3f5ff980d246fcd5fa5640d5d06123828287200
antepavlinusic <ante.pavlinusic@gmail.com>
2024-05-27 23:39:20 +0200

    Vjezba Urar_silvija
    Vjezbanje kucanja koda urar_silvija

---
+++ urar_silvija.sql
@@ -0,0 +1,23 @@
+--create database urara_silvija;
+
+--create table korisnik(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--id_popravka int
+--);
+
+--create table sat(
+--sifra int,
+--model varchar(50),
+--godina_proizvodnje datetime,
+--serijski_broj int
+--);
+
+--create table popravak(
+--sifra int,
+--sat int,
+--cijena decimal(18,2),
+--segrt bit
+--);
+
